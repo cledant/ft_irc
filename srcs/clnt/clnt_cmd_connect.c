4aa6b99a2163074bfbdbf31bfc1aadfe900dab69
cledant <cledant@student.42.fr>
2017-04-19 19:12:24 +0200

    norm on clnt_cmd_connect

--- srcs/clnt/clnt_cmd_connect.c
+++ srcs/clnt/clnt_cmd_connect.c
@@ -6,37 +6,54 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 18:32:40 by cledant           #+#    #+#             */
-/*   Updated: 2017/04/19 18:47:07 by cledant          ###   ########.fr       */
+/*   Updated: 2017/04/19 19:10:33 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_irc.h"
 
-int		clnt_cmd_connect(const t_cmd_arg *arg, t_clnt_env *env)
+static inline void		no_port(char *first_arg, t_clnt_env *env,
+							const t_cmd_arg *arg, const char *ptr_first_arg)
+{
+	ft_memcpy(first_arg, ptr_first_arg + 1, arg->end - ptr_first_arg);
+	if (env->state == CONNECTED)
+		clnt_disconnect(env);
+	if (clnt_connect_server(first_arg, DEFAULT_PORT, env) != ERR_NONE)
+	{
+		wprintw(env->out, "\nCouldn't connect to server !");
+		clnt_disconnect(env);
+	}
+}
+
+static inline int		check_first_arg(char **ptr_first_arg, t_clnt_env *env,
+							const t_cmd_arg *arg)
+{
+	if ((*ptr_first_arg = ft_strchr(arg->begin, ' ')) == NULL)
+	{
+		wprintw(env->out, "\nNot enough arguments for /connect");
+		return (0);
+	}
+	if (arg->end - *ptr_first_arg == 0)
+	{
+		wprintw(env->out, "\nNot enough arguments for /connect");
+		return (0);
+	}
+	return (1);
+}
+
+int						clnt_cmd_connect(const t_cmd_arg *arg, t_clnt_env *env)
 {
 	char	*ptr_first_arg;
 	char	*ptr_second_arg;
 	char	first_arg[MAX_MSG_LEN + 1];
 	char	second_arg[MAX_MSG_LEN + 1];
 
-	if ((ptr_first_arg = ft_strchr(arg->begin, ' ')) == NULL)
-	{
-		wprintw(env->out, "\nNot enough arguments for /connect");
+	if (check_first_arg(&ptr_first_arg, env, arg) == 0)
 		return (0);
-	}
 	ft_bzero(first_arg, MAX_MSG_LEN + 1);
 	ft_bzero(second_arg, MAX_MSG_LEN + 1);
 	if ((ptr_second_arg = ft_strchr(ptr_first_arg + 1, ' ')) == NULL)
-	{
-		ft_memcpy(first_arg, ptr_first_arg + 1, arg->end - ptr_first_arg);
-		if (env->state == CONNECTED)
-			clnt_disconnect(env);
-		if (clnt_connect_server(first_arg, DEFAULT_PORT, env) != ERR_NONE)
-		{
-			wprintw(env->out, "\nCouldn't connect to server !");
-			clnt_disconnect(env);
-		}
-	}
+		no_port(first_arg, env, arg, ptr_first_arg);
 	else
 	{
 		ft_memcpy(first_arg, ptr_first_arg + 1,
