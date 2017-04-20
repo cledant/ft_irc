3f123c82d639264091893b048acb831862a1d898
cledant <cledant@student.42.fr>
2017-04-20 18:23:31 +0200

    Norme on clnt_disp_second_cmd_check

--- srcs/clnt/clnt_disp_second_cmd_check.c
+++ srcs/clnt/clnt_disp_second_cmd_check.c
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 12:50:56 by cledant           #+#    #+#             */
-/*   Updated: 2017/04/20 16:33:08 by cledant          ###   ########.fr       */
+/*   Updated: 2017/04/20 18:22:23 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,26 @@ static inline int		is_welcome_cmd(const char *cmd_part, t_disp_cmd *cmd)
 	return (1);
 }
 
-int		clnt_disp_second_cmd_check(t_disp_cmd *cmd, const t_cmd_arg *arg)
+static inline int		first_check(t_disp_cmd *cmd, const t_cmd_arg *arg,
+							size_t *len, char **ptr_space_one)
+{
+	if ((*ptr_space_one = ft_strnstr(arg->begin, " ",
+			arg->end - arg->begin - 1)) == NULL)
+		return (0);
+	if ((*len = arg->end - *ptr_space_one - 1) == 0)
+		return (0);
+	return (1);
+}
+
+int						clnt_disp_second_cmd_check(t_disp_cmd *cmd,
+							const t_cmd_arg *arg)
 {
 	char	*ptr_space_one;
 	char	*ptr_space_two;
 	char	cmd_part[MAX_CMD_SIZE + 1];
 	size_t	len;
 
-	if ((ptr_space_one = ft_strnstr(arg->begin, " ",
-			arg->end - arg->begin - 1)) == NULL)
-		return (0);
-	if ((len = arg->end - ptr_space_one - 1) == 0)
+	if (first_check(cmd, arg, &len, &ptr_space_one) == 0)
 		return (0);
 	ft_bzero(&cmd_part, MAX_CMD_SIZE + 1);
 	if ((ptr_space_two = ft_strnstr(ptr_space_one + 1, " ",
