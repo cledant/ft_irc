42c200880025095cd6c500ac6945f0237c07373f
cledant <cledant@student.42.fr>
2017-05-01 15:13:39 +0200

    fix srv_seek_chan_id not checking channel state

--- srcs/srv/srv_seek_chan_id.c
+++ srcs/srv/srv_seek_chan_id.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 17:58:08 by cledant           #+#    #+#             */
-/*   Updated: 2017/03/29 18:37:32 by cledant          ###   ########.fr       */
+/*   Updated: 2017/05/01 14:53:04 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int		srv_seek_chan_id(t_env *env, const char *chan_name)
 	c = 0;
 	while (c < MAX_NB_CHAN)
 	{
-		if (ft_strcmp(chan_name, env->list_chan[c].name) == 0)
+		if (env->list_chan[c].state == CHAN_IN_USE &&
+				ft_strcmp(chan_name, env->list_chan[c].name) == 0)
 			return (c);
 		c++;
 	}
